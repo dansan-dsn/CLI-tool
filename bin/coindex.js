db6b690d30efb32fb6eb7fbe2825997ab235f8ce
dansan-dsn <ddryn970@gmail.com>
2024-05-23 23:52:47 +0300

    cli

--- bin/coindex.js
+++ bin/coindex.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-// const program = require('commander')
+const program = require('commander')
 
 console.log('hello from coindex')
 
-// console.log(process.argv)
+console.log(process.argv)
